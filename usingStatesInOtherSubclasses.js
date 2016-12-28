70e4a0988db31b69d28bf55084cf031b8dff0da0
Elgskred <christoffer.antonsen@gmail.com>
2016-12-28 21:16:35 +0100

    How to properly access and set states in subclasses

---
+++ usingStatesInOtherSubclasses.js
@@ -0,0 +1,37 @@
+//codepen.io
+class Test extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "abc",
+      name: "Karl",
+      age: 22
+    };
+  }
+
+  logCmd = () => {
+    console.log(this.state.value);
+    this.setState({
+      value: 'Hello'
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.logCmd}>
+          {this.state.value}
+        </button> <br />
+        Name: {this.state.name}  <br />
+
+        Age of {this.state.name} is {this.state.age} <br />
+      </div>
+    );
+  }
+  
+}
+
+ReactDOM.render(
+  <Test />,
+  document.getElementById('container')
+);
