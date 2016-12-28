9c2f22b59e97e22e367b00ff0684043a54779a8c
Elgskred <christoffer.antonsen@gmail.com>
2016-12-28 22:51:02 +0100

    Added comments

--- FormEventHandlingAndFormSubmit.js
+++ FormEventHandlingAndFormSubmit.js
@@ -1,19 +1,22 @@
 class Test extends React.Component {
   constructor() {
     super();
+    //Inital data
     this.state = {
       navn: "",
       beskrivelse: "",
       oppskrift: ""
     };
   }
+  //Using arrowfunction to catch onChange events for each field in the form
+  //Using 'id' to identify them
   onChange = (e) => {
     var t = {};
     var id = e.target.id
     t[id] = e.target.value
     this.setState(t);
   };
-
+  //Preventing default submit behaviour so we can add our own
   submitForm = (e) => {
     e.preventDefault();
     console.log(this.state);
@@ -24,6 +27,7 @@ class Test extends React.Component {
       <div>
         <form onSubmit={this.submitForm}>
           <br />
+          //'id' is important for event identification so we don't need one function for each field
           <input type="text" placeholder="Oppskrift" id="navn" onChange={this.onChange} value={this.state.navn}/>
           <br />
 
